8c0e85fcd8b6ecda57d8f15e1bb20a7c5a27e514
andresdvx <andresdavidpachecocuadro@gmail.com>
2025-02-04 12:46:04 -0500

    update: enhance Select component with onSelectChange handler and add CheckIcon

--- src/components/Select/SelectItem.tsx
+++ src/components/Select/SelectItem.tsx
@@ -1,12 +1,25 @@
-export interface SelectItemProps {
+import CheckIcon from "./svg/CheckIcon";
+
+export interface SelectItemProps extends React.LiHTMLAttributes<HTMLLIElement> {
   label: string;
   value: string;
+  selectedItem?: { label: string; value: string; } | undefined;
 }
 
-const SelectItem = ({ label, value }: SelectItemProps) => {
-  return <option data-custom="true" value={value} className="">{label}</option>;
+const SelectItem = ({ label, value, selectedItem, ...props }: SelectItemProps) => {
+  return (
+    <li
+      {...props}
+      className="rounded-md flex items-center cursor-pointer"
+    >
+      <div className="w-full flex justify-between !p-2 rounded-sm hover:bg-gray-300 transition-all duration-500">
+        <p className="text-sm">{label}</p>
+        <p className="text-sm">
+          {selectedItem && selectedItem.value === value ? <CheckIcon /> : null}
+        </p>
+      </div>
+    </li>
+  );
 };
 
 export default SelectItem;
-
-
