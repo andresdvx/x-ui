31dfd8095f483cb105d2cd23d75add9da2ae8934
andresdvx <andresdavidpachecocuadro@gmail.com>
2025-02-10 17:47:03 -0500

    fix: Select - Sharp transition when an item is first selected

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -38,7 +38,7 @@ export const Select = ({
   return (
     <div
       {...props}
-      className={`relative bg-white select-none border-1 border-[#8c8c8c] hover:border-[#0484ff] rounded-md transition-all duration-300 ${className} `}
+      className={`relative bg-white select-none border-1 border-[#8c8c8c] hover:border-[#0484ff] hover:shadow-[0_0_3px_#0484ff] rounded-md transition-all duration-300 ${className} `}
     >
       <div
         onClick={() => setOpen(!open)}
@@ -47,15 +47,26 @@ export const Select = ({
         aria-haspopup="listbox"
         aria-expanded={open}
       >
-        <p className={`h-[50%] text-md !px-3 text-[#8c8c8c] transition-all duration-200 ${(open && !selected) && '-translate-y-2 text-sm'} ${selected && 'text-sm'}`}>{label}</p>
-      { selected && <p className="h-[50%] text-sm text-left !px-3">{selected ? selected.label : ""}</p>}
+    <p
+      className={`
+        absolute left-3 transition-all duration-300 
+        ${selected || open ? "top-0.5 text-sm text-[#8c8c8c]" : "top-1/2 -translate-y-1/2 text-md text-gray-500"}
+      `}
+    >
+      {label}
+    </p>
+    {selected && (
+      <p className="absolute top-6 left-3 text-sm text-gray-800">
+        {selected.label}
+      </p>
+    )}
       </div>
 
       {open && (
         <ul
-          className={`absolute w-full max-h-42 ${items.length > 5 && 'overflow-y-scroll'} scroll-smooth !mt-1 flex flex-col gap-1 !p-1 bg-white rounded-md shadow-lg`}
+          className={`absolute w-full max-h-42 ${items.length > 4 && 'overflow-y-scroll'} scroll-smooth !mt-1 flex flex-col gap-1 !p-1 bg-white rounded-md shadow-lg`}
           role="listbox"
-          style={{msOverflowStyle: 'none', scrollbarWidth: 'none', WebkitOverflowScrolling: 'touch'}}
+          style={{ msOverflowStyle: 'none', scrollbarWidth: 'none', WebkitOverflowScrolling: 'touch' }}
         >
           {items.map((item) => (
             <SelectItem
